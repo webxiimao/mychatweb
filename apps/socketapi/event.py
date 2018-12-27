2143d1e2e096a6e31f64e117995f8586edf6053f
webxiimao <web_xiimao@outlook.com>
2018-12-27 17:04:09 +0800

    add websocket

--- apps/socketapi/event.py
+++ apps/socketapi/event.py
@@ -2,7 +2,21 @@
 '''
 write by xii
 '''
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO,join_room,leave_room
 
 
 socketio = SocketIO()
+
+
+@socketio.on('connect')
+def client_connect():
+    socketio.emit('client_connect',{'data':"sucess"})
+
+
+
+# 用户上线自动加入各有id的房间
+@socketio.on('server_connent')
+def server_connent(userInfo):
+    room = userInfo['id']
+    join_room(room)
+    socketio.emit('client_connect',{'data':"success%s"%(userInfo['username'])},room=room)
