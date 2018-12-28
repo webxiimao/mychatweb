07b9b90b43d6121c36b7e8a5cc2eea7cc284dc99
webxiimao <web_xiimao@outlook.com>
2018-12-28 15:37:22 +0800

    完成websocket接口好友互发消息的部分

--- apps/socketapi/event.py
+++ apps/socketapi/event.py
@@ -20,3 +20,14 @@ def server_connent(userInfo):
     room = userInfo['id']
     join_room(room)
     socketio.emit('client_connect',{'data':"success%s"%(userInfo['username'])},room=room)
+
+
+#客户端发送消息
+@socketio.on('send msg')
+def send_msg(data):
+    print(data)
+    if 'src' in data.keys() :
+        socketio.emit('client_get_msg', data, room=data['src'])
+        socketio.emit('client_get_msg', data, room=data['des'])
+    else:
+        socketio.emit('client_get_msg', data)
